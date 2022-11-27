2edb27fe522938de8297787e23246b5d3ef2be71
Donovan Romaya <donovanromaya@gmail.com>
2022-11-27 18:30:10 -0500

    added more code

--- CAI.py
+++ CAI.py
@@ -1,19 +1,64 @@
-mode = 0
-
-
+import random
 def menu(n):
-    if 1 <= n <= 4:
-        return int(n)
+    good = ["Keep up the great work!","Excellent!","Great job!"]
+    
+    bad = ["No.Wrong answer.Keep trying.","Wrong.try again.","Wrong.Keep trying."]
+    
+    good = 0
+    
+    bad = 0
+    
+    dif = input("Welcome, Please enter the difficulty level(1 or 2): ")
+    if dif == "1" or dif == "2":
+        while(1):
+            print("1 = Addition")
+            print("2 = Subtraction")
+            print("3 = Multiplication")
+            print("4 = Division")
+            print("5 = Random")
+            chal = input("Enter the challenge you'd like to try: ")
+            if chal == "5":
+                op = str(random.randint(1,4))
+            num1 = 0
+            num2 = 0
+            res = 0
+            ans = 0
+            if dif == "1":
+                num1 = random.randint(0,9)
+                num2 = random.randint(0,9)
+            else:
+                num1 = random.randint(10,99)
+                num2 = random.randint(10,99)
+            if num1<num2:
+                sw = num1
+                num1 = num2
+                num2 = sw
+            elif chal == "-1":
+                leave(good,bad)
+            elif chal == "1":
+              print(f'How much is {num1}+{num2}?')
+              res = num1 + num2
+              
+            elif chal == "2":
+              print(f'How much is {num1}-{num2}?')
+              res = num1 - num2
+              
+            elif chal == "3":
+              print(f'How much is {num1}*{num2}?')
+              res = num1 * num2
+              
+            elif chal == "4":
+                if num2 == 0:
+                    sw = num1
+                    num1 = num2
+                    num2 = sw
+                print(f'How much is {num1}//{num2}?')
+                res = num1 // num2
+            if ans == res:
+                print(good[random.randint(0,2)])
+                good = good +1
+            else:
+                print(bad[random.randint(0,2)])
+                bad = bad + 1
 
 
-print("Enter option")
-print("1: Addition")
-print("2: Subtraction")
-print("3: Multiplication")
-print("4: Division")
-
-choice = menu(int(input("Choice: ")))
-while choice is None:
-    choice = menu(int(input("Please enter an actual value: ")))
-
-print("You chose " + str(choice))
---
+++ Email Address Python Project.py
@@ -0,0 +1,37 @@
+def menu():
+    print("Menu")
+    print("--------------------")
+    print("1. Look up an email")
+    print("2. Add a new name and email address")
+    print("3. Change an existing email address")
+    print("4. Delete a name and email address")
+    print("5. Quit")
+    opt = input("Enter your option: ")
+    
+    #while(1):
+    
+    #while(2):
+
+    #while(3):
+
+    #while(4):
+
+    while(5):
+        print("information saved")
+        exit()
+        
+    return(n)
+
+#def lookUp():
+
+#def addEmail():
+
+#def changeEmail():
+
+#def deleteEmail():
+
+#def saveEmailToFile():
+
+def main():
+    menu()
+main()
