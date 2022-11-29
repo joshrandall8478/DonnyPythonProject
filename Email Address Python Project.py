4e74257dfe1ee2fa285a381eef2553250ff91437
josh <joshrandall8478@gmail.com>
2022-11-29 17:06:17 -0500

    Fixing code

--- Email Address Python Project.py
+++ Email Address Python Project.py
@@ -5,33 +5,38 @@ def menu():
     print("2. Add a new name and email address")
     print("3. Change an existing email address")
     print("4. Delete a name and email address")
-    print("5. Quit") 
+    print("5. Quit")
+    opt = int(input("Enter the command you'd like to use: "))
+    return opt
 
-def lookUpEmail(name,em):
+
+def lookupEmail(file):
     name = str(input("Enter a name: "))
-    if name in em:
-            print("Name: {}".format(name))
-            print("Email: {}".format(email))
-            print("")
+    if name in file:
+        print("Name: {}".format(name))
+        print("Email: {}".format(file))
+        print("")
     else:
         print("Name has not been found")
 
-def addEmail(email):
+
+def addEmail(file):
     name = str(input("Enter a name: "))
     email = str(input("Enter an email address: "))
-    em.append(name)
+    file.append(name)
     print("Name and email have been added")
     print("")
 
-def changeEmail(email):
+
+def changeEmail(file):
     name = str(input("Enter name: "))
     if name not in email:
-            print("name not found")
-            return None
+        print("name not found")
+        return None
     email = str(input("Enter new email address"))
-    em.append(name)
+    file.append(name)
     print("")
-    
+
 
 def deleteEmail(email):
     name = str(input("Enter name: "))
@@ -39,43 +44,44 @@ def deleteEmail(email):
         print("name not found")
         print("")
         return None
-    del em[name]
+    del email[name]
     print("information deleted")
     print("")
-    
+
 
 def saveEmail(email):
-    file = open("EmailAddresses.txt","r")
-    names = list(em.keys())
+    file = open("EmailAddresses.txt", "r")
+    names = list(email.keys())
     for i in range(len(names)):
-        file.write(names[i] + " " + em[names[i]])
-        if len(names)-i > 1:
+        file.write(names[i] + " " + email[names[i]])
+        if len(names) - i > 1:
             file.write("\n")
-    
+
 
 def read(email):
-    file = em.read()
+    file = email.read()
     for i in range(len(file)):
         ent = file[i].split(" ")
     return email
 
+
 def main():
-    email = open("EmailAddresses.txt")
+    file = open("EmailAddresses.txt", "a")
     run = True
     while run:
         menu()
-        opt = int(input("Enter the command you'd like to use: "))
-        if opt == 1:
-                  lookUpEmail(em)
-        if opt == 2:
-                  addEmail(em)
-        if opt == 3:
-                  changeEmail(em)
-        if opt == 4:
-                  deleteEmail(em)
-        if opt == 5:
-                  saveEmail(em)
-                  print("information saved")
-                  run = False
-main()
+        if menu() == 1:
+            lookupEmail(file)
+        if menu() == 2:
+            addEmail(file)
+        if menu() == 3:
+            changeEmail(file)
+        if menu() == 4:
+            deleteEmail(file)
+        if menu() == 5:
+            saveEmail(file)
+            print("information saved")
+            run = False
 
+
+main()
